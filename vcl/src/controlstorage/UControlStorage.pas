8ee97061a736b11e33329367ad1773890cbc349f
Holger Flick <holger@flixengineering.com>
2023-07-24 18:24:31 -0400

    storage of forms and splitters

--- vcl/src/controlstorage/UControlStorage.pas
+++ vcl/src/controlstorage/UControlStorage.pas
@@ -26,16 +26,54 @@ uses
   ;
 
 type
+  TControlList = TList<TControl>;
+
+  TCSControl = class;
+  TCSControls = TList<TCSControl>;
+
   [Automapping, Entity]
   TCSControl = class
   private
+    FId: Integer;
+
+    [Column('Y')]
     FTop: Integer;
+
+    [Column('X')]
     FLeft: Integer;
+
+    [Column('W')]
     FWidth: Integer;
+
+    [Column('H')]
     FHeight: Integer;
+
+    [Column('Name', [TColumnProp.Unique],500)]
+    FName: String;
+
+    [Association([], CascadeTypeAllButRemove)]
+    FOwner: TCSControl;
+
+    [ManyValuedAssociation([TAssociationProp.Lazy], CascadeTypeAll, 'FOwner')]
+    FChildren: Proxy<TCSControls>;
+    function GetChildren: TCSControls;
+    procedure SetChildren(const Value: TCSControls);
+
   public
+    constructor Create;
+    destructor Destroy; override;
+
+    procedure UpdateFromControl(AControl: TControl);
+
+    property Id: Integer read FId write FId;
+
+    property Name: String read FName write FName;
+
+    property Owner: TCSControl read FOwner write FOwner;
+    property Children: TCSControls read GetChildren write SetChildren;
+
     property Top: Integer read FTop write FTop;
-    property Left: Integer read FLeft write FLeft;
+    property Left: Integer read FLeft write FTop;
     property Width: Integer read FWidth write FWidth;
     property Height: Integer read FHeight write FHeight;
   end;
@@ -62,7 +100,6 @@ type
     property Visible: Boolean read FVisible write FVisible;
   end;
 
-
   TCSDBGridColumns = TList<TCSDBGridColumn>;
 
   [Automapping, Entity]
@@ -71,6 +108,7 @@ type
     [ManyValuedAssociation([TAssociationProp.Lazy], CascadeTypeAll, 'FGrid')]
     FColumns: Proxy<TCSDBGridColumns>;
     FId: Integer;
+
     function GetColumns: TCSDBGridColumns;
     procedure SetColumns(const Value: TCSDBGridColumns);
   public
@@ -78,10 +116,9 @@ type
     destructor Destroy; override;
 
     property Id: Integer read FId write FId;
+
     property Columns: TCSDBGridColumns read GetColumns write SetColumns;
   end;
-
-  TControlList = TList<TWinControl>;
 
   TFormStorageManager = class
   strict private
@@ -91,7 +128,7 @@ type
     FObjectManager: TObjectManager;
 
     procedure AddControlsToList(
-      AControl: TWinControl;
+      AControl: TControl;
       AList: TControlList );
 
   public
@@ -104,12 +141,17 @@ type
     procedure StoreForm( AForm: TForm );
     procedure RestoreForm( AForm: TForm );
 
+    property ObjectManager: TObjectManager read FObjectManager;
   end;
 
 implementation
 
 uses
-    UDataManager
+    UDictionary
+  , UDataManager
+  , Vcl.StdCtrls
+  , Vcl.ExtCtrls
+  , Vcl.DBGrids
   ;
 
 { TCSDBGrid }
@@ -152,9 +194,69 @@ begin
   inherited;
 end;
 
-procedure TFormStorageManager.RestoreForm(AForm: TForm);
+procedure TFormStorageManager.StoreForm(AForm: TForm);
+var
+  LList: TControlList;
+  LName: String;
+
 begin
-  // restore specific form controls
+  // check if form has been stored before -- name is unique
+  // form is a control like any other - just has children
+
+  var LForm := ObjectManager.Find<TCSControl>
+    .Where(Dic.CSControl.Name = 'Form.' + AForm.Name )
+    .UniqueResult
+    ;
+
+  if not Assigned(LForm) then
+  begin
+    LForm := TCSControl.Create;
+    LForm.Name := 'Form.' + AForm.Name;
+    ObjectManager.Save(LForm);
+  end;
+
+  LForm.UpdateFromControl( AForm );
+
+  // get a flattened list of all controls on the form
+  LList := TControlList.Create;
+  try
+    AddControlsToList(AForm, LList );
+
+    for var LControl in LList do
+    begin
+      if LControl = AForm then
+      begin
+        continue;   // skip the form
+      end;
+
+
+      // we have to do this for each control
+      if (LControl is TSplitter)  then
+      begin
+        // check if control is already in list of form
+        var LChild := ObjectManager.Find<TCSControl>
+          .Where(
+            (Dic.CSControl.Owner.Id = LForm.Id) AND
+            (Dic.CSControl.Name = LControl.Name)
+            )
+          .UniqueResult
+          ;
+
+        if not Assigned(LChild) then
+        begin
+          LChild := TCSControl.Create;
+          LChild.Name := LControl.Name;
+          LForm.Children.Add(LChild);
+        end;
+
+        LChild.UpdateFromControl( LControl );
+      end;
+    end;
+  finally
+    LList.Free;
+  end;
+
+  ObjectManager.Flush;
 end;
 
 class function TFormStorageManager.Shared: TFormStorageManager;
@@ -167,12 +269,12 @@ begin
   Result := FInstance;
 end;
 
-procedure TFormStorageManager.StoreForm(AForm: TForm);
+procedure TFormStorageManager.RestoreForm(AForm: TForm);
 begin
-// store specific form controls
+  // restore specific form controls
 end;
 
-procedure TFormStorageManager.AddControlsToList(AControl: TWinControl;
+procedure TFormStorageManager.AddControlsToList(AControl: TControl;
   AList: TControlList);
 begin
   if not Assigned( AControl ) then
@@ -182,14 +284,22 @@ begin
 
   AList.Add(AControl);
 
-  if AControl.ControlCount > 0 then
+  if AControl is TWinControl then
   begin
-    for var i := 0 to AControl.ControlCount -1 do
+    var LWinControl := TWinControl( AControl );
+
+    if LWinControl.ControlCount > 0 then
     begin
-      var LControl := AControl.Controls[i];
-      if LControl is TWinControl then
+      for var i := 0 to LWinControl.ControlCount -1 do
       begin
-        AddControlsToList( AControl.Controls[i] as TWinControl, AList );
+        var LControl := LWinControl.Controls[i];
+
+        // forms hosted on panels will trigger their own
+        // storage and should not be stored as a child of the form hosting them
+        if not (LControl is TForm) then
+        begin
+          AddControlsToList( LWinControl.Controls[i], AList );
+        end;
       end;
     end;
   end;
@@ -202,8 +312,43 @@ begin
   FObjectManager := TDataManager.Shared.ObjectManager;
 end;
 
+{ TCSControl }
+
+constructor TCSControl.Create;
+begin
+  inherited;
+
+  FChildren.SetInitialValue(TCSControls.Create);
+end;
+
+destructor TCSControl.Destroy;
+begin
+  FChildren.DestroyValue;
+
+  inherited;
+end;
+
+function TCSControl.GetChildren: TCSControls;
+begin
+  Result := FChildren.Value;
+end;
+
+procedure TCSControl.SetChildren(const Value: TCSControls);
+begin
+  FChildren.Value := Value;
+end;
+
+procedure TCSControl.UpdateFromControl(AControl: TControl);
+begin
+  self.Left := AControl.Left;
+  self.Top := AControl.Top;
+  self.Width := AControl.Width;
+  self.Height := AControl.Height;
+end;
+
 initialization
   RegisterEntity(TCSControl);
   RegisterEntity(TCSDBGrid);
+  RegisterEntity(TCSDBGridColumn);
 
 end.
